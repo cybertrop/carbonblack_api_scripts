dbb689467a7df6c5e5c6df402c7fdffa147165ce
John Doe <39686917+cybertrop@users.noreply.github.com>
2019-03-01 13:28:40 -0800

    Create cb_api_binary_search_output_data_json_parser.sh

---
+++ cb_api_binary_search_output_data_json_parser.sh
@@ -0,0 +1,12 @@
+# This bash script will take your CB API binary_search.py output data
+# and parse it into a JSON format for further data ingestion or what have you
+
+file="$1"
+for x in file; do
+  cat binary_search_data.txt |
+  sed "s/u'/'/g" |
+  sed "s/'/\"/g" |
+  sed "s/True/\"True\"/g" |
+  grep -Ei "{.*}" |
+  sed "s/Original Document    : //g"
+done
